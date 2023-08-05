45693a0ef97ce828440536b633872c72cd8d8f62
tron2004 <tron2004@users.noreply.github.com>
2023-08-05 01:24:25 -0600

    Agregando todo el proyecto challenge 1 Alura

---
+++ index.js
@@ -0,0 +1,69 @@
+
+function encriptar(){
+
+    let texto = document.getElementById("textoEncriptar").value;
+
+
+
+    let textoCifrado = texto
+        .replace(/e/gi , "enter")
+        .replace(/i/gi , "imes")
+        .replace(/a/gi , "ai")
+        .replace(/o/gi , "ober")
+        .replace(/u/gi , "ufat");
+
+    if (document.getElementById("textoEncriptar").value.length != 0) {
+        document.getElementById("textoEncriptar").value = textoCifrado;
+        document.getElementById("exito").textContent = "Texto encriptado con éxito";
+        document.getElementById("tituloMensaje").textContent ="";
+        document.getElementById("parrafo").textContent ="";
+        mostrarLike();
+        
+        
+    } else{
+
+        alert("Debes de ingresar algun texto");
+
+    }
+}
+
+
+function desencriptar(){
+
+    let texto = document.getElementById("textoEncriptar").value;
+
+    let textoCifrado = texto
+        .replace(/enter/gi,"e")
+        .replace(/imes/gi , "i")
+        .replace(/ai/gi , "a")
+        .replace(/ober/gi,"o")
+        .replace(/ufat/gi, "u")
+
+    if (document.getElementById("textoEncriptar").value.length != 0) {
+        document.getElementById("textoEncriptar").value = textoCifrado;
+        document.getElementById("exito").textContent = "Texto desencriptado con éxito";
+        document.getElementById("exito").style.left = "15%" 
+        document.getElementById("tituloMensaje").textContent ="";
+        document.getElementById("parrafo").textContent ="";
+        mostrarLike();
+    } else{
+    
+        alert("Debes de ingresar algun texto");
+    
+    }
+
+}
+
+function copiar(){
+    mensaje.select();
+    navigator.clipboard.writeText(mensaje.value)
+    mensaje.value = "";
+    alert("Texto Copiado")
+}
+
+function mostrarLike(){
+
+    document.getElementById("lupita").style.display = "none";
+    document.getElementById("like").style.display = "flex";
+
+}
